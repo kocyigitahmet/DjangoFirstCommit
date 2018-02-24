41f968807f2202f763418f871a092afb9a252781
kocyigit <kocyigitahmet@icloud.com>
2018-02-24 21:56:29 +0300

    Django icin ilk commitim

---
+++ welcome.py
@@ -0,0 +1,28 @@
+#today=2018
+#age=today-born
+#print(age)
+
+def me(name, surname):
+
+    if name=='ahmet' and surname=='kocyigit':
+        print('bu benim')
+    elif name=='mehmet':
+        print('selam mehmet ' + surname)
+    else:
+        print("isim yok")
+
+me('mehmet','demir')
+
+
+def hi(girl):
+    print('hi '+name2 +'!')
+
+girls = ['mary','sue','anna','rose','kernel']
+for name2 in girls:
+    hi(name2)
+    print('siradaki')
+
+print('bitti')
+
+for i in range(1,6):
+    print(i)
